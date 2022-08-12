69d200f6a2d1b4dd68e3339bf26f7225f5481be4
jaggerjacs <jaggerthejacs@gmail.com>
2022-08-12 16:36:57 -0300

    ut

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,18 +1,28 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
 import { ApiService } from './api.service';
+import { of } from 'rxjs';
 
 describe('ApiService', () => {
   let service: ApiService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: []
+      imports: [HttpClientTestingModule],
+      providers: [HttpClient]
     });
-    // service = TestBed.inject(ApiService);
+    service = TestBed.inject(ApiService);
   });
 
   it('should be created', () => {
-    // expect(service).toBeTruthy();
+    expect(service).toBeTruthy();
   });
+
+  it('should call api', () => {
+    const spy = spyOn(service, 'getAllProducts');
+    service.getAllProducts()
+    expect(spy).toHaveBeenCalled();
+  });
+
 });
