bb48e6ff695ab0e7a1261e1d832d46127be50d07
jaggerjacs <jaggerthejacs@gmail.com>
2022-08-12 16:37:19 -0300

    ut

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -2,7 +2,6 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { HttpClient } from '@angular/common/http';
 import { ApiService } from './api.service';
-import { of } from 'rxjs';
 
 describe('ApiService', () => {
   let service: ApiService;
